55ed64e45d81c941108b0d8ff6643202d72a858c
Justin-ZS <jzstxdy@gmail.com>
2020-10-10 15:55:39 +0800

    feat: add more aggregators

--- src/modules/aggregate.ts
+++ src/modules/aggregate.ts
@@ -2,13 +2,14 @@ import {
   ITable, Visitor, AggregateType, FieldDescription,
   DataType, TableData, TableMeta, AggregateDescription,
 } from 'Typings';
-import { once } from 'PureUtils';
 import { ArrayColumn } from './column';
 
+// #region aggregators
 abstract class Aggregator {
-  readonly type: AggregateType;
   readonly field: FieldDescription;
   readonly name: string;
+
+  type: AggregateType;
   dataType = DataType.Null;
 
   constructor(targetField: FieldDescription, name: string) {
@@ -16,20 +17,18 @@ abstract class Aggregator {
     this.name = name;
   }
 
-  abstract init(): void;
   abstract addUp(value: unknown): Aggregator;
   abstract get value(): unknown;
 }
 
 class SumAggregator extends Aggregator {
   type = AggregateType.Sum;
-
-  private sum: number;
-  init() {
-    this.sum = 0;
-    this.dataType = DataType.Number;
-  }
+  dataType = DataType.Number;
+  private sum: number = null;
+  
   addUp(value: number) {
+    if (value == null) return this;
+
     this.sum += +value; // @TODO: handle type coerce
     return this;
   }
@@ -37,11 +36,95 @@ class SumAggregator extends Aggregator {
     return this.sum;
   }
 }
+class MinAggregator extends Aggregator {
+  type = AggregateType.Min;
+  dataType = DataType.Number;
+  private min: number = null;
+
+  addUp(value: number) {
+    if (value == null) return this;
+
+    if (this.min == null) {
+      this.min = value;
+    } else {
+      this.min = Math.min(this.min, +value); // @TODO: handle type coerce
+    }
+
+    return this;
+  }
+  get value() {
+    return this.min;
+  }
+}
+class MaxAggregator extends Aggregator {
+  type = AggregateType.Max;
+  dataType = DataType.Number;
+  private max: number = null;
+
+  addUp(value: number) {
+    if (value == null) return this;
+
+    if (this.max == null) {
+      this.max = value;
+    } else {
+      this.max = Math.max(this.max, +value); // @TODO: handle type coerce
+    }
+
+    return this;
+  }
+  get value() {
+    return this.max;
+  }
+}
+class CountAggregator extends Aggregator {
+  type = AggregateType.Count;
+  dataType = DataType.Number;
+  private count = 0;
+
+  addUp() {
+    this.count++;
+    return this;
+  }
+  get value() {
+    return this.count;
+  }
+}
+class AvgAggregator extends Aggregator {
+  type = AggregateType.Avg;
+  dataType = DataType.Number;
+  private sumAgg = new SumAggregator(this.field, this.name);
+  private cntAgg = new CountAggregator(this.field, this.name);
+
+  addUp(value: number) {
+    this.sumAgg.addUp(value);
+    this.cntAgg.addUp();
+
+    return this;
+  }
+  get value() {
+    const sum = this.sumAgg.value;
+    const count = this.cntAgg.value;
+
+    if (count === 0) return null;
+    if (sum == null) return null;
+
+    return sum/count;
+  }
+}
+// #endregion
 
 const getAggregatorByDescription = (aggDesc: AggregateDescription): Aggregator => {
   switch (aggDesc.type) {
     case AggregateType.Sum:
       return new SumAggregator(aggDesc.field, aggDesc.name);
+    case AggregateType.Min:
+      return new MinAggregator(aggDesc.field, aggDesc.name);
+    case AggregateType.Max:
+      return new MaxAggregator(aggDesc.field, aggDesc.name);
+    case AggregateType.Count:
+      return new CountAggregator(aggDesc.field, aggDesc.name);
+    case AggregateType.Avg:
+      return new AvgAggregator(aggDesc.field, aggDesc.name);
     default:
       throw Error(`Not Supported Aggregate Type: ${aggDesc.type}`);
   }
@@ -52,10 +135,10 @@ export const getAggregatedTable = (
   table: ITable,
 ) => {
   const aggs = aggDescs.map(getAggregatorByDescription);
-  const initOnce = once(() => aggs.forEach(agg => agg.init()));
+  // const initOnce = once(() => aggs.forEach(agg => agg.init()));
 
   const visitor: Visitor = (rowIdx) => {
-    initOnce();
+    // initOnce();
     aggs.forEach(agg => agg.addUp(table.getCell(agg.field.name, rowIdx)));
   };
   table.traverse(visitor);
